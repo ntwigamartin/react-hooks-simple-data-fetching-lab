923b89fb7361ff8fe1daf8dcd420580274e8a6bd
ntwigamartin <ntwiga.martin@gmail.com>
2023-01-17 23:52:07 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React from "react";
+
+function App(){
+    const [displayDog, setDisplayDog] = React.useState(false)
+    
+    React.useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then(data=>{
+            setDisplayDog(data.message)
+        })
+    },[])
+    
+    const element = displayDog ? <img src={displayDog} alt="A Random Dog"/> : <p>Loading...</p>;
+    
+    
+    return (
+        <div>
+            {element}
+            
+        </div>
+    )
+}
+
+export default App;
